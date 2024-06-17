5068665bd8bab3d103db7d8c4a6cc5db3a3a17f9
merkol <merterkol@outlook.com>
2024-06-17 22:27:47 +0300

    update

--- dncm/__init__.py
+++ dncm/__init__.py
@@ -1,5 +1,7 @@
 from .dncm import DNCM
 from .dncm import Encoder
+from .dncm import MobileViTv2Attention
+from .dncm import DeNIM_StyleSwap_to_Canon
 
 
-__all__ = ["DNCM", "Encoder"]
+__all__ = ["DNCM", "Encoder", "MobileViTv2Attention", "DeNIM_StyleSwap_to_Canon"]
